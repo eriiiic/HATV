5f85c7d4fc54cba5c706de72eeadcc8064a00834
Eric Delattre <eric@delattre.me>
2026-04-01 08:23:57 +0700

    Initial Commit

---
+++ HATV/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HATV
+//
+//  Created by Eric on 01/04/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ HATVTests/HATVTests.swift
@@ -0,0 +1,19 @@
+//
+//  HATVTests.swift
+//  HATVTests
+//
+//  Created by Eric on 01/04/2026.
+//
+
+import Testing
+@testable import HATV
+
+struct HATVTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
